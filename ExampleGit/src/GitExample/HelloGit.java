c11b1483e0bd96dc3950dcf14b9197ea4112940c
ismayil1678 <ismayil@na.edu>
2020-07-26 17:16:58 -0500

    Add year

--- ExampleGit/src/GitExample/HelloGit.java
+++ ExampleGit/src/GitExample/HelloGit.java
@@ -4,8 +4,9 @@ public class HelloGit {
 	
 public static  void  main(String args[]){
 	String name="Jenkins";
+	int year=2020;
 		
-		System.out.println("Name:"+name);
+		System.out.println("Name:"+name+":"+year);
  }
 
 }
